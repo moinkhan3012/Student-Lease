02aed4c158d3d8ea1081ab9163e13be3a40efb28
virjhangianinyu <vrj2006@nyu.edu>
2024-05-04 18:35:51 -0400

    Changes to creat-sublet and view sublet, new functions for marketplace creat and view added

--- lambdas/view-marketplace/lambda_function.py
+++ lambdas/view-marketplace/lambda_function.py
@@ -9,9 +9,9 @@ from geopy.exc import GeocoderTimedOut
 from boto3.dynamodb.conditions import Key
 import io
 # https://search-search-test-elastic1-dedivdy53hkcwdyfce4yy7m36m.aos.us-east-1.on.aws
-esUrl = "https://search-search-test-elastic1-dedivdy53hkcwdyfce4yy7m36m.aos.us-east-1.on.aws/sublets/_search"
+esUrl = "https://search-search-test-elastic1-dedivdy53hkcwdyfce4yy7m36m.aos.us-east-1.on.aws/marketplace/_search"
 dynamo = boto3.resource('dynamodb')
-dynamodb = dynamo.Table('Listings')
+dynamodb = dynamo.Table('Marketplace')
 s3 = boto3.client('s3')
 bucket_name = 'studentlease-listing-images'
 
@@ -37,16 +37,12 @@ def lambda_handler(event, context):
     lat, lon = geocode_address(search_params['location'])
     # Perform search in Elasticsearch
     body={
-        "_source": ['id'],
-
         "query": {
             "bool": {
                 "must": [
-                    {"match": {"bedrooms": search_params['bedrooms']}},
-                    {"range": {"monthlyRent": {"lte": int(search_params['monthlyRent'])}}},
-                    {"range": {"bathrooms": {"gte": int(search_params['bathrooms'])}}},
-                    {"range": {"dateAvailable": {"gte": search_params['dateAvailable']}}},
-                    {"range": {"leaseDuration": {"gte": search_params['leaseDuration']}}}
+                    {"match": {"labels": search_params['label']}},
+                    {"range": {"price": {"lte": int(search_params['price'])}}}
+                    
                 ],
                 "filter": {
                     "geo_distance": {
@@ -67,7 +63,6 @@ def lambda_handler(event, context):
         headers=headers,
         auth=HTTPBasicAuth('', '')
     )
-    
     # # Extract apartment IDs from search results
     print('search_result', search_result.json())
     apartment_details = []
